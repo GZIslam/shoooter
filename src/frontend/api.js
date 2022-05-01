9887f81258ff2fd8eb5c9eafac0925a6255387cc
Gaibullaev <gaibullaeviz@gmail.com>
2022-05-01 18:13:08 +0300

    feat: add dev config

--- src/frontend/api.js
+++ src/frontend/api.js
@@ -1,3 +1,4 @@
 export const getRooms = () => {
-    return fetch('https://api.shooter.online/rooms').then(res => res.json()).catch(e => console.error(e));
+    const host = import.meta.env.MODE === "dev" ? "http://127.0.0.1:3000" : "https://api.shoooter.online";
+    return fetch(`${host}/rooms`).then(res => res.json()).catch(e => console.error(e));
 };
